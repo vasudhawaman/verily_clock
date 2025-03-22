9dfd0e89ec392645334b80a76cafab9efbfc06f0
vasudhawaman <144978373+vasudhawaman@users.noreply.github.com>
2025-03-23 00:19:17 +0530

    Create test_bench_duty.v

---
+++ test_bench_duty.v
@@ -0,0 +1,26 @@
+module tb;
+  wire clk1;
+  wire clk2;
+  wire clk3;
+  wire clk4;
+  reg  enable;
+  reg [7:0] dly;
+
+  clock_gen u0(enable, clk1);
+  clock_gen #(.DUTY(25)) u1(enable, clk2);
+  clock_gen #(.DUTY(75)) u2(enable, clk3);
+  clock_gen #(.DUTY(90)) u3(enable, clk4);
+
+  initial begin
+    enable <= 0;
+
+    for (int i = 0; i < 10; i= i+1) begin
+      dly = $random;
+      #(dly) enable <= ~enable;
+      $display("i=%0d dly=%0d", i, dly);
+      #50;
+    end
+
+    #50 $finish;
+  end
+endmodule
